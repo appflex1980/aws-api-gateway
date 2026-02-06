ee761ea77ffff82c60714e7b571ed3fd4a5de083
Yevgeni Shapiro <23049337+appflex1980@users.noreply.github.com>
2026-02-06 23:28:13 +0200

    update

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,4 @@
 
-
 resource "random_id" "this" {
   byte_length = 4
 }
@@ -12,13 +11,31 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "this" {
+resource "aws_s3_bucket_website_configuration" "this" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "AllowPublicRead"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.this.arn}/*"
+    }]
+  })
 }
 
 resource "aws_s3_object" "index" {
