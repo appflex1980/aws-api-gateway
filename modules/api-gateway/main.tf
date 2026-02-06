b77d1be9c3071c5ef3735ed7a46594208ca7ac90
Yevgeni Shapiro <23049337+appflex1980@users.noreply.github.com>
2026-02-06 23:14:08 +0200

    Update main.tf

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -1,1 +1,70 @@
 
+resource "aws_api_gateway_rest_api" "site_api" {
+  name = "s3-website-proxy"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.site_api.id
+  parent_id   = aws_api_gateway_rest_api.site_api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.site_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.site_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  type                    = "HTTP_PROXY"
+  integration_http_method = "ANY"
+
+  uri = "http://${aws_s3_bucket.site.bucket}.s3-website-us-east-1.amazonaws.com/{proxy}"
+
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+}
+
+resource "aws_api_gateway_method" "root" {
+  rest_api_id   = aws_api_gateway_rest_api.site_api.id
+  resource_id   = aws_api_gateway_rest_api.site_api.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "root" {
+  rest_api_id = aws_api_gateway_rest_api.site_api.id
+  resource_id = aws_api_gateway_rest_api.site_api.root_resource_id
+  http_method = aws_api_gateway_method.root.http_method
+
+  type                    = "HTTP_PROXY"
+  integration_http_method = "ANY"
+
+  uri = "http://${aws_s3_bucket.site.bucket}.s3-website-us-east-1.amazonaws.com"
+}
+
+resource "aws_api_gateway_deployment" "deploy" {
+  rest_api_id = aws_api_gateway_rest_api.site_api.id
+
+  depends_on = [
+    aws_api_gateway_integration.proxy,
+    aws_api_gateway_integration.root
+  ]
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  deployment_id = aws_api_gateway_deployment.deploy.id
+  rest_api_id   = aws_api_gateway_rest_api.site_api.id
+  stage_name    = "prod"
+}
+
