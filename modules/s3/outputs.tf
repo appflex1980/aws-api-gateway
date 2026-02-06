a4c3299d2d113de7366a967719e6c0dfc01b2db3
Yevgeni Shapiro <23049337+appflex1980@users.noreply.github.com>
2026-02-06 23:33:06 +0200

    update

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,6 +1,6 @@
 
 output "bucket_name" {
-  value = aws_s3_bucket.this.id
+  value = aws_s3_bucket.this.bucket
 }
 
 output "bucket_arn" {
@@ -9,4 +9,8 @@ output "bucket_arn" {
 
 output "bucket_regional_domain_name" {
   value = aws_s3_bucket.this.bucket_regional_domain_name
+}
+
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.this.website_endpoint
 }
