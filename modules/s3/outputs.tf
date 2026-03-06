17e4c87d219bea03e096e1ead8f841349042943c
Yevgeni Shapiro <yevgeni.shapiro@gmail.com>
2026-03-06 16:24:42 +0000

    update

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -11,9 +11,15 @@ output "bucket_regional_domain_name" {
   value = aws_s3_bucket.this.bucket_regional_domain_name
 }
 
+#output "website_endpoint" {
+#  value = aws_s3_bucket.this.website_endpoint
+#}
+
+
 output "website_endpoint" {
-  value = aws_s3_bucket.this.website_endpoint
+  value = aws_s3_bucket_website_configuration.this.website_endpoint
 }
+
 
 #output "website_endpoint" {
 #  value = aws_s3_bucket_website_configuration.this.website_endpoint
